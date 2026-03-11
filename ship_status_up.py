9e4947f219ffb8626316e6772d4d7b06f70ae5d9
KPainter-MJL <kpainter@mjl-enterprises.com>
2026-03-11 17:07:18 -0400

    Added sql engine initialization code

--- ship_status_up.py
+++ ship_status_up.py
@@ -3,6 +3,11 @@ import json
 import os
 import time
 import datetime
+import pyodbc
+import nacl.secret
+import nacl.utils
+from sqlalchemy import create_engine, text, event
+from sqlalchemy.engine import URL
 
 CLIENTID = os.environ.get('FED_PUB_KEY')
 SECRETKEY = os.environ.get('FED_SEC_KEY')
@@ -70,6 +75,22 @@ def fetchTrackingStatus(TrackingNum, accessToken):
 accessTokenRemainingTime = 0
 accessToken = None
 updateRate = 60
+
+# Will require testing in MJL remote environment
+"""
+# Prompt for password and create database connection
+creds = None
+
+with "" as key_f:
+	with "" as df_f:
+		box = nacl.secret.SecretBox(key_f.read())
+		creds = box.decrypt(df_f.read()).decode('utf-8')
+
+if creds != None:
+	connection_string = f"Driver={{SQL Server Native Client 11.0}};Server=localhost;UID=sa;PWD={creds};Database=MJLTest"
+	connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+	engine = create_engine(connection_url, fast_executemany=True)
+"""
 
 # Loop until break
 while True:
