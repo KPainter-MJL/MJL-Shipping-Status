729d2cecbbb36be5151d9fcd4d70463f9e5ff923
KPainter-MJL <kpainter@mjl-enterprises.com>
2026-03-11 15:37:29 -0400

    Add minor logging and main execution loop logic

--- ship_status_up.py
+++ ship_status_up.py
@@ -1,52 +1,88 @@
 import requests
 import json
+import os
+import time
+import datetime
 
-ClientID = ""
-SecretKey = ""
+CLIENTID = os.environ.get('FED_PUB_KEY')
+SECRETKEY = os.environ.get('FED_SEC_KEY')
 
-OAuthURL = "https://apis-sandbox.fedex.com/oauth/token"
+OAUTHURL = "https://apis-sandbox.fedex.com/oauth/token"
+TRACKURL = "https://apis-sandbox.fedex.com/track/v1/trackingnumbers"
 
-# Structure OAuth Request
-AuthHeader = {
-    'Content-Type': "application/x-www-form-urlencoded"
-}
+# NOTE: Find a way to gracefully exit on restart, sigterm, sigint, sigkill
 
-AuthReq = { 
-    "grant_type": "client_credentials",
-    "client_id": ClientID, 
-    "client_secret": SecretKey
-}
+# TODO: Error checking
+def fetchAuthToken():
 
-# Request and separate access token for later use
-AuthResponse = requests.post(OAuthURL, data=AuthReq, headers=AuthHeader)
-AccessToken = json.loads(AuthResponse.text)["access_token"]
+    if CLIENTID == None or SECRETKEY == None:
+        print("API Credentials Not Found")
+        exit()
 
+    # Structure OAuth Request
+    authHeader = {
+        'Content-Type': "application/x-www-form-urlencoded"
+    }
 
-# Fetch tracking numbers here
-TrackingNum = ""
+    authReq = { 
+        "grant_type": "client_credentials",
+        "client_id": CLIENTID, 
+        "client_secret": SECRETKEY
+    }
 
-TrackURL = "https://apis-sandbox.fedex.com/track/v1/trackingnumbers"
+    # Request and separate access token for later use
+    authResponse = requests.post(OAUTHURL, data=authReq, headers=authHeader)
+    accessToken = json.loads(authResponse.text)["access_token"]
+    
+    return accessToken
 
-# Should probably process a request for each tracking number unless they can be done in bulk
-TrackHeader = {
-    "content-type": "application/json",
-    "authorization": "Bearer " + AccessToken
-}
+# TODO: Error checking
+def fetchTrackingStatus(TrackingNum, accessToken):
 
-TrackReq = {
-    "includeDetailedScans": True,
-    "trackingInfo": 
-    [
-        {
-            "trackingNumberInfo": 
+    # Fetch tracking numbers here
+    # TrackingNum = "499496853570"
+
+    # Should probably process a request for each tracking number unless they can be done in bulk
+    trackHeader = {
+        "content-type": "application/json",
+        "authorization": "Bearer " + accessToken
+    }
+
+    trackReq = {
+        "includeDetailedScans": True,
+        "trackingInfo": 
+        [
             {
-                "trackingNumber": TrackingNum
+                "trackingNumberInfo": 
+                {
+                    "trackingNumber": trackingNum
+                }
             }
-        }
-    ]
-}
+        ]
+    }
 
-# Request and process tracking information
-TrackingResponse = requests.post(TrackURL, data=json.dumps(TrackReq), headers=TrackHeader)
+    # Request and process tracking information
+    trackingResponse = requests.post(TRACKURL, data=json.dumps(trackReq), headers=trackHeader)
 
-print(TrackingResponse.text)
+    return trackingResponse
+
+# Define remaining access time and desired update rate
+accessTokenRemainingTime = 0
+accessToken = None
+updateRate = 60
+
+# Loop until break
+while True:
+
+    if accessTokenRemainingTime <= updateRate:
+        accessToken = fetchAuthToken()
+        accessTokenRemainingTime = 3600
+        print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] Renewed OAuth Token")
+
+    # Should grab relevant records here and store in array/dict keying off tracking numbers
+    print(f"Current remaining auth time is {accessTokenRemainingTime} seconds")
+    # Iterate over items in aforementioned record dict, updating database records as we go
+
+    time.sleep(updateRate)
+    accessTokenRemainingTime = accessTokenRemainingTime - updateRate
+
