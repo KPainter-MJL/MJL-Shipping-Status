e8d8ef8ae0691747816b298abe046094e428985d
KPainter-MJL <kpainter@mjl-enterprises.com>
2026-03-18 17:13:32 -0400

    Add basic exception handling

--- ship_status_up.py
+++ ship_status_up.py
@@ -9,18 +9,20 @@ import nacl.utils
 from sqlalchemy import create_engine, text, event
 from sqlalchemy.engine import URL
 
-CLIENTID = os.environ.get('FED_PUB_KEY')
-SECRETKEY = os.environ.get('FED_SEC_KEY')
+CLIENT = os.environ.get('FED_PUB_KEY')
+SECRET = os.environ.get('FED_SEC_KEY')
 
 OAUTHURL = "https://apis-sandbox.fedex.com/oauth/token"
 TRACKURL = "https://apis-sandbox.fedex.com/track/v1/trackingnumbers"
 
 # NOTE: Find a way to gracefully exit on restart, sigterm, sigint, sigkill
 
-# TODO: Error checking
-def fetchAuthToken():
+"""
+Returns String OAuth access token
+"""
+def fetchAuthToken(clientID, secretKey):
 
-    if CLIENTID == None or SECRETKEY == None:
+    if clientID == None or secretKey == None:
         print("API Credentials Not Found")
         exit()
 
@@ -31,17 +33,23 @@ def fetchAuthToken():
 
     authReq = { 
         "grant_type": "client_credentials",
-        "client_id": CLIENTID, 
-        "client_secret": SECRETKEY
+        "client_id": clientID, 
+        "client_secret": secretKey
     }
 
     # Request and separate access token for later use
-    authResponse = requests.post(OAUTHURL, data=authReq, headers=authHeader)
+    try:
+        authResponse = requests.post(OAUTHURL, data=authReq, headers=authHeader)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
     accessToken = json.loads(authResponse.text)["access_token"]
     
     return accessToken
 
-# TODO: Error checking
+"""
+Returns JSON tracking status when provided with a tracking number and access token in the form of strings
+"""
 def fetchTrackingStatus(TrackingNum, accessToken):
 
     # Fetch tracking numbers here
@@ -67,7 +75,10 @@ def fetchTrackingStatus(TrackingNum, accessToken):
     }
 
     # Request and process tracking information
-    trackingResponse = requests.post(TRACKURL, data=json.dumps(trackReq), headers=trackHeader)
+    try:
+        trackingResponse = requests.post(TRACKURL, data=json.dumps(trackReq), headers=trackHeader)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
 
     return trackingResponse
 
@@ -76,27 +87,45 @@ accessTokenRemainingTime = 0
 accessToken = None
 updateRate = 60
 
-# Will require testing in MJL remote environment
+# NOTE: Will require testing in remote environment
 """
-# Prompt for password and create database connection
-creds = None
-
-with "" as key_f:
-	with "" as df_f:
-		box = nacl.secret.SecretBox(key_f.read())
-		creds = box.decrypt(df_f.read()).decode('utf-8')
-
-if creds != None:
-	connection_string = f"Driver={{SQL Server Native Client 11.0}};Server=localhost;UID=sa;PWD={creds};Database=MJLTest"
-	connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-	engine = create_engine(connection_url, fast_executemany=True)
+Returns SQL engine object when provided with MJL DB credentials in the form of a string
 """
+def connectMJLDatabase():
+
+    creds = None
+    # NOTE: Will have to find a more permanent residence for the encrypted credentials if we want to continue using this
+    # solution
+    with open("", "br") as key_f:
+        with open("", "br") as df_f:
+            box = nacl.secret.SecretBox(key_f.read())
+            creds = box.decrypt(df_f.read()).decode('utf-8')
+
+    if creds != None:
+        connection_string = f"Driver={{SQL Server Native Client 11.0}};Server=localhost;UID=sa;PWD={creds};Database=MJLTest"
+        connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+        engine = create_engine(connection_url, fast_executemany=True)
+        return engine
+    else:
+        print("Error parsing credentials")
+
+"""
+Returns list of tracking numbers from provided database engine
+"""
+def fetchOpenOrderTrackingNumbers(engine):
+    # Query
+
+    # Parsing returned records
+
+    # trackingNumbers = list of tuples or dict
+    
+    return trackingNumbers
 
 # Loop until break
 while True:
 
     if accessTokenRemainingTime <= updateRate:
-        accessToken = fetchAuthToken()
+        accessToken = fetchAuthToken(CLIENT, SECRET)
         accessTokenRemainingTime = 3600
         print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] Renewed OAuth Token")
 
